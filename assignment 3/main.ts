6eefa87ffbd38c07f910d2d74883874092d4c562
Habiba Khan <122211697+Habiba-K@users.noreply.github.com>
2024-03-22 18:17:20 +0500

    Add files via upload

---
+++ assignment 3/main.ts
@@ -0,0 +1,16 @@
+/* Name Cases: Store a person’s name in a variable, 
+and then print that person’s name in lowercase, uppercase, and titlecase. */
+
+ /*  the forward slash(/) is represent starting of regular expression
+    \b represent boundary (each empty string represents a word boundary)  
+    \w represent characters of the word
+    /g represent global 
+    => arrow function
+    */
+
+let personName: string = "habiba khan";
+console.log(personName.toUpperCase());
+console.log(personName.toLowerCase());
+
+let TitleCase= personName.replace(/\b\w/g, match => match.toUpperCase());
+console.log(TitleCase); 
