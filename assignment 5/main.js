823063d18bd450299b13c35876b79fab01522065
Habiba Khan <122211697+Habiba-K@users.noreply.github.com>
2024-03-22 18:18:19 +0500

    Add files via upload

---
+++ assignment 5/main.js
@@ -0,0 +1,7 @@
+"use strict";
+/* store the famous person’s name in a variable called famous_person.
+Then compose your message and store it in a new variable called message. Print your message.
+ */
+let famous_person = "Dwayne Johnson";
+let message = `${famous_person} once said, "Success isn't always about greatness. It's about consistency. Consistent hard work leads to success. Greatness will come."`;
+console.log(message);
---
+++ assignment 5/main.ts
@@ -0,0 +1,8 @@
+/* store the famous person’s name in a variable called famous_person. 
+Then compose your message and store it in a new variable called message. Print your message.
+ */
+
+
+let famous_person : string =  "Dwayne Johnson";
+let message : string = `${famous_person} once said, "Success isn't always about greatness. It's about consistency. Consistent hard work leads to success. Greatness will come."`;
+console.log(message);
