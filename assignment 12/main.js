84d4be02f294a397678445833e4eeea42a7fb2ae
Habiba Khan <122211697+Habiba-K@users.noreply.github.com>
2024-03-22 18:22:04 +0500

    Add files via upload

---
+++ assignment 12/main.js
@@ -0,0 +1,7 @@
+"use strict";
+/* printing each person’s name with a message to them. The text of each message should
+be the same, but each message should be personalized with the person’s name.*/
+let names = ["Sara", "Sana", "Rida", "Aniza"];
+for (let i = 0; i < names.length; i++) {
+    console.log(`${names[i]} likes to play cricket`);
+}
