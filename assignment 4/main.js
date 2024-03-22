1847e91c01f004452320c658c47e152edd5ba609
Habiba Khan <122211697+Habiba-K@users.noreply.github.com>
2024-03-22 18:17:44 +0500

    Add files via upload

---
+++ assignment 4/main.js
@@ -0,0 +1,6 @@
+"use strict";
+/* Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+Your output should look something like the following, including the quotation marks:
+
+Albert Einstein once said, “A person who never made a mistake never tried anything new.” */
+console.log(`Dwayne Johnson once said, "Success isn't always about greatness. It's about consistency. Consistent hard work leads to success. Greatness will come."`);
---
+++ assignment 4/main.ts
@@ -0,0 +1,7 @@
+/* Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+Your output should look something like the following, including the quotation marks:
+
+Albert Einstein once said, “A person who never made a mistake never tried anything new.” */
+
+
+console.log(`Dwayne Johnson once said, "Success isn't always about greatness. It's about consistency. Consistent hard work leads to success. Greatness will come."`);
