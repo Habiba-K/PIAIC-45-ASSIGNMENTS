21ac5d6a8f236c8a1b4c8233b512db1e004a8d17
Habiba Khan <122211697+Habiba-K@users.noreply.github.com>
2024-03-22 18:19:48 +0500

    Add files via upload

---
+++ assignment 8/main.js
@@ -0,0 +1,14 @@
+"use strict";
+/* You should create four lines that look like this:
+
+_____________________________________________
+
+console.log(5 + 3)
+
+_____________________________________________
+
+Your output should simply be four lines with the number 8 appearing once on each line. */
+console.log(4 + 4);
+console.log(12 - 4);
+console.log(2 * 4);
+console.log(32 / 4);
