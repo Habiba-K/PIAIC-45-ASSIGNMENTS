7d1e81d932528b13e321aeb1b27d91674101b37a
Habiba Khan <122211697+Habiba-K@users.noreply.github.com>
2024-03-22 18:19:11 +0500

    Add files via upload

---
+++ assignment 7/main.js
@@ -0,0 +1,11 @@
+"use strict";
+/* Number Eight: Write addition, subtraction, multiplication, and division operations that each
+result in the number 8. Be sure to enclose your operations in print statements to see the results. */
+let add = 4 + 4;
+let sub = 12 - 4;
+let mul = 2 * 4;
+let div = 32 / 4;
+console.log(`Addition :  ${add}`);
+console.log(`Subtraction :  ${sub}`);
+console.log(`Multiply :  ${mul}`);
+console.log(`Divide :  ${div}`);
