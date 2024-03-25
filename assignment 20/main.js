6bac241b644fffaacd2660c593a44dd8c652a0e6
Habiba Khan <122211697+Habiba-K@users.noreply.github.com>
2024-03-25 12:08:51 +0500

    Update main.js

--- assignment 20/main.js
+++ assignment 20/main.js
@@ -1,5 +1,6 @@
 "use strict";
-// to print array
+/* Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything
+else you’d like. Write a program that creates a list containing these items.*/
 let languages = ["HTML", "CSS", "Javascript", "Typescript", "Python"];
 console.log("List of Programming Languages\n");
 languages.forEach(lang => {
