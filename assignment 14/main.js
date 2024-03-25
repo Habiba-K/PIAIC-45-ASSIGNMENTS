5cada9b493716140fcda65108d67fb23049766b7
Habiba Khan <122211697+Habiba-K@users.noreply.github.com>
2024-03-25 11:58:35 +0500

    Update main.js

--- assignment 14/main.js
+++ assignment 14/main.js
@@ -1,7 +1,7 @@
 "use strict";
 /* Make a list that includes at least three people you’d like to invite to dinner.
 Then use your list to print a message to each person, inviting them to dinner. */
-const GuestList = ["Aniza", "Maryam", "Aaish"];
+let GuestList = ["Aniza", "Maryam", "Aaish"];
 GuestList.forEach(guest => {
     console.log(`Dear ${guest}, You are invited to dinner at my place.`);
 });
