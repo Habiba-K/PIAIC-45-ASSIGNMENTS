57220b439f5f06cb2558675afd271682a20e73eb
Habiba Khan <122211697+Habiba-K@users.noreply.github.com>
2024-03-25 11:58:16 +0500

    Update main.ts

--- assignment 14/main.ts
+++ assignment 14/main.ts
@@ -1,7 +1,7 @@
 /* Make a list that includes at least three people you’d like to invite to dinner. 
 Then use your list to print a message to each person, inviting them to dinner. */
 
-const GuestList: string[] = ["Aniza", "Maryam", "Aaish"];
+let GuestList: string[] = ["Aniza", "Maryam", "Aaish"];
 
 
 GuestList.forEach(guest => {
