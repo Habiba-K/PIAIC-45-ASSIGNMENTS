b34c2666e247c461aa5243af4cf25c1b55fde40c
Habiba Khan <122211697+Habiba-K@users.noreply.github.com>
2024-03-25 12:06:57 +0500

    Update main.js

--- assignment 19/main.js
+++ assignment 19/main.js
@@ -1,6 +1,6 @@
 "use strict";
 // print the number of people that invited for dinner through array
-const GuestList = ["Nabiha", "Aniza", "Sonia", "Muniba", "Aaish", "Zubia"];
+let GuestList = ["Nabiha", "Aniza", "Sonia", "Muniba", "Aaish", "Zubia"];
 let count = 0;
 while (count < GuestList.length) {
     count++;
